074752e824b9ce5640e5c7f75e8412d9f938d13a
Aashi858 <72612631+Aashi858@users.noreply.github.com>
2021-05-30 12:27:51 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
-music = "";
-music1 = "";
-music2 = "";
 show_yourself = "";
 let_it_go = "";
+rightWristX = "";
+rightWristY = "";
+leftWristX = "";
+leftWristY = "";
 
 function preload(){
-    music = loadSound("music.mp3");
-    music1 = loadSound("music1.mp3");
-    music2 = loadSound("music2.mp3");
     let_it_go = loadSound("let it go.mp3");
     show_yourself = loadSound("show_yourself.mp3");
 }
@@ -17,7 +15,24 @@ function setup(){
 
     video =  createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video,model_loaded);
+    poseNet.on("pose",got_results);
 }
 function draw(){
     image(video,0,0,500,300);
+}
+function model_loaded(){
+    console.log("Model is loaded");
+}
+function got_results(results){
+    if(results.length > 0){
+        console.log(results);
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("Right_Wrist X :" + rightWristX + " Right_Wrist_Y :" + rightWristY);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("Lift_Wrist X :" + leftWristX + " Left_Wrist_Y :" + leftWristY);
+    }
 }
