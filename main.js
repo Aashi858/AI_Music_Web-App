dec2c5f0ef0259d0a9a2a6ddef1bbb6f09f7c5b5
Aashi858 <72612631+Aashi858@users.noreply.github.com>
2021-06-04 19:13:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ rightWristX = "";
 rightWristY = "";
 leftWristX = "";
 leftWristY = "";
+score_left_wrist = "";
 
 function preload(){
     let_it_go = loadSound("let it go.mp3");
@@ -11,7 +12,7 @@ function preload(){
 }
 function setup(){
     canvas = createCanvas(500,300);
-    canvas.position(400,250);
+    canvas.position(400,260);
 
     video =  createCapture(VIDEO);
     video.hide();
@@ -21,6 +22,14 @@ function setup(){
 }
 function draw(){
     image(video,0,0,500,300);
+    if(score_left_wrist > 0.2){
+        circle(leftWristX,leftWristY,30);
+        fill("#7a8cf0");
+        stroke("#7a8cf0");
+        show_yourself.play();
+        show_yourself.setVolume(1);
+        document.getElementById("song_name").innerHTML = "Show Yourself";
+    }
 }
 function model_loaded(){
     console.log("Model is loaded");
@@ -28,9 +37,14 @@ function model_loaded(){
 function got_results(results){
     if(results.length > 0){
         console.log(results);
+
+        score_left_wrist = results[0].pose.keypoints[9].score;
+        console.log("Score Left Wrist:" + score_left_wrist);
+
         rightWristX = results[0].pose.rightWrist.x;
         rightWristY = results[0].pose.rightWrist.y;
         console.log("Right_Wrist X :" + rightWristX + " Right_Wrist_Y :" + rightWristY);
+
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("Lift_Wrist X :" + leftWristX + " Left_Wrist_Y :" + leftWristY);
